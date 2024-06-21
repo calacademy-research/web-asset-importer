6de4ccfa8b26368a59c0c4145eafd8d0d5b1e9fb
delarocamcas <mdelaroca@calacademy.org>
2024-06-21 14:30:10 -0700

    optional remarks, moved internal md5 path back to internal_filename field

--- ichthyology_importer.py
+++ ichthyology_importer.py
@@ -121,7 +121,8 @@ class IchthyologyImporter(Importer):
         filepath_list = self.remove_imagedb_imported_filenames_from_list(filepath_list)
         filepath_list = self.clean_duplicate_image_barcodes(filepath_list)
         # TODO: hardcoded user ID
-        self.import_to_imagedb_and_specify(filepath_list, collection_object_id, 68835, skip_redacted_check=True)
+        self.import_to_imagedb_and_specify(filepath_list, collection_object_id, 68835, skip_redacted_check=True,
+                                           fill_remarks=False)
 
 
 #         If I find a .jpg, import it.
