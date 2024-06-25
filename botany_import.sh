f0830b135b6dd3c1614437be12b2230dc2494d53
delarocamcas <mdelaroca@calacademy.org>
2024-06-25 10:15:20 -0700

    updated import .sh scripts with lockfiles

--- botany_import.sh
+++ botany_import.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/bash
-cd "$(dirname "$0")"
-source ./env/bin/activate
-python3 ./client_tools.py -vvvv Botany import >& botany_import_log.txt &
+LOCKFILE=/tmp/botany_import.lock
 
+cleanup() {
+    rm -f "$LOCKFILE"
+}
 
+if [ -e "$LOCKFILE" ]; then
+    echo "Botany import is already running. Exiting."
+    exit 1
+else
+    # Create the lock file
+    touch "$LOCKFILE"
+
+    # trap to ensure lock file is removed on script exit
+    trap cleanup EXIT
+
+    # Main import process
+    cd "$(dirname "$0")" || exit
+    source ./env/bin/activate
+    python3 ./client_tools.py -vvvv Botany import >& botany_import_log.txt &
+fi
